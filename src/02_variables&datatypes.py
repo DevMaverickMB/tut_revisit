2542fcb1a0df82f3d4f201c601af08e7126565dc
DevMaverickMB <84549389+DevMaverickMB@users.noreply.github.com>
2026-03-17 22:04:27 +0530

    day2

---
+++ src/02_variables&datatypes.py
@@ -0,0 +1,33 @@
+a = 10
+t = type(a)
+print(a, "is of type", t) #10 is of type <class 'int'>
+
+#Q1
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+c = a + b
+
+print(c)
+print ("Average of", a, "and", b, "is", (a+b)/2)
+
+#Q2
+x = int(input("Enter number which needs to be divided: "))
+y = x%2
+print(y)
+
+#Q3
+a = input("Enter the variable: ")
+b = type(a)
+print(b)
+
+#Q4
+a = 34
+b = 80
+
+print(a > b)
+
+#Q5
+a = int(input("Enter the number which you want to see square of: "))
+b = a*a
+print(b)
