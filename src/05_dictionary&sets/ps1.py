69e2227aef7a7f0c59fe3c55ed02bbc83d5f4e2d
DevMaverickMB <84549389+DevMaverickMB@users.noreply.github.com>
2026-03-25 19:22:50 +0530

    day 5

---
+++ src/05_dictionary&sets/ps1.py
@@ -0,0 +1,10 @@
+dict = {
+    "Kursi": "Chair",
+    "Kutta": "Dog",
+    "Log": "People",
+    "Aadmi": "Person"
+}
+
+hindi = input("Enter the hindi word you want to search: ")
+
+print(dict[hindi], "is the english word for", hindi)
---
+++ src/05_dictionary&sets/ps6.py
@@ -0,0 +1,23 @@
+d = {}
+
+name = input("Enter your name: ")
+language = input("Enter your language: ")
+
+d.update({name: language})
+name = input("Enter your name: ")
+language = input("Enter your language: ")
+
+d.update({name: language})
+name = input("Enter your name: ")
+language = input("Enter your language: ")
+
+d.update({name: language})
+name = input("Enter your name: ")
+language = input("Enter your language: ")
+
+d.update({name: language})
+
+print(d)
+
+# d[name] = language 
+# This is also valid for assignment
